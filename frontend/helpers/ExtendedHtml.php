728ec1b5afa9686c9ab39c95034e60c5932d3841
walter <anatoliy.biliy@gmail.com>
2015-12-17 10:31:00 +0200

    add comments to ExtendedHtml

--- frontend/helpers/ExtendedHtml.php
+++ frontend/helpers/ExtendedHtml.php
@@ -8,9 +8,20 @@ namespace frontend\helpers;
 
 use yii\helpers\Html;
 
+/**
+ * Class ExtendedHtml
+ * @package frontend\helpers
+ */
 class ExtendedHtml extends Html
 {
 
+    /**
+     * Generates ajax hyperlink tag
+     * @param $text
+     * @param null $url
+     * @param array $options
+     * @return string
+     */
     public static function ajaxLink($text, $url = null, $options = [])
     {
         $options['class'] = isset($options['class'])
@@ -21,6 +32,13 @@ class ExtendedHtml extends Html
         return parent::a($text, $url, $options);
     }
 
+    /**
+     * Generates a hyperlink tag to another sites
+     * @param $text
+     * @param null $url
+     * @param array $options
+     * @return string
+     */
     public static function externalLink($text, $url = null, $options = [])
     {
         $options['rel'] = 'noindex/nofollow';
