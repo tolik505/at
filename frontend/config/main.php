431f8d05f3a134bd1ab7f054151900ec2b473b5f
walter <anatoliy.biliy@gmail.com>
2015-12-18 16:37:57 +0200

    add sitemap config example

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -16,7 +16,30 @@ return [
         'sitemap' => [
             'class' => 'himiklab\sitemap\Sitemap',
             'models' => [
-                // your models
+                //your models
+                /*
+                'app\modules\news\models\News',
+                //or configuration for creating a behavior
+                [
+                    'class' => 'app\modules\news\models\News',
+                    'behaviors' => [
+                        'sitemap' => [
+                            'class' => SitemapBehavior::className(),
+                            'scope' => function ($model) {
+                                    $model->select(['url', 'lastmod']);
+                                    $model->andWhere(['is_deleted' => 0]);
+                                },
+                            'dataClosure' => function ($model) {
+                                    return [
+                                        'loc' => Url::to($model->url, true),
+                                        'lastmod' => strtotime($model->lastmod),
+                                        'changefreq' => SitemapBehavior::CHANGEFREQ_DAILY,
+                                        'priority' => 0.8
+                                    ];
+                                }
+                        ],
+                    ],
+                ],*/
             ],
             'urls'=> [
                 [
@@ -25,6 +48,33 @@ return [
                     'changefreq' => \himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
                     'priority' => 0.8
                 ],
+                // your additional urls
+                /*[
+                    'loc' => '/news/index',
+                    'changefreq' => \himiklab\sitemap\behaviors\SitemapBehavior::CHANGEFREQ_DAILY,
+                    'priority' => 0.8,
+                    'news' => [
+                        'publication'   => [
+                            'name'          => 'Example Blog',
+                            'language'      => 'en',
+                        ],
+                        'access'            => 'Subscription',
+                        'genres'            => 'Blog, UserGenerated',
+                        'publication_date'  => 'YYYY-MM-DDThh:mm:ssTZD',
+                        'title'             => 'Example Title',
+                        'keywords'          => 'example, keywords, comma-separated',
+                        'stock_tickers'     => 'NASDAQ:A, NASDAQ:B',
+                    ],
+                    'images' => [
+                        [
+                            'loc'           => 'http://example.com/image.jpg',
+                            'caption'       => 'This is an example of a caption of an image',
+                            'geo_location'  => 'City, State',
+                            'title'         => 'Example image',
+                            'license'       => 'http://example.com/license',
+                        ],
+                    ],
+                ],*/
             ],
             'cacheKey' => 'sitemapCacheKey',
             'enableGzip' => false, // default is false
