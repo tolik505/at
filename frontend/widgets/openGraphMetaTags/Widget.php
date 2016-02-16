54c441799e36d5385464ae0457a390e0f9150991
pavelnaumenko <pavel@vintage.com.ua>
2016-02-16 09:50:07 +0200

    COD-51 remove duplicate var request

--- frontend/widgets/openGraphMetaTags/Widget.php
+++ frontend/widgets/openGraphMetaTags/Widget.php
@@ -37,18 +37,20 @@ class Widget extends \yii\base\Widget
      */
     public function run()
     {
+        $view = $this->getView();
+
 //        $this->getView()->registerMetaTag(['property' => 'fb:app_id', 'content' => '']);
-        $this->getView()->registerMetaTag(['property' => 'og:title', 'content' => $this->title]);
-        $this->getView()->registerMetaTag(['property' => 'og:description', 'content' => $this->description]);
-        $this->getView()->registerMetaTag(['property' => 'og:url', 'content' => $this->url]);
-        $this->getView()->registerMetaTag(['property' => 'og:image', 'content' => $this->image]);
-        $this->getView()->registerMetaTag(['property' => 'og:image:width', 'content' => 1024]);
-        $this->getView()->registerMetaTag(['property' => 'og:image:height', 'content' => 512]);
+        $view->registerMetaTag(['property' => 'og:title', 'content' => $this->title]);
+        $view->registerMetaTag(['property' => 'og:description', 'content' => $this->description]);
+        $view->registerMetaTag(['property' => 'og:url', 'content' => $this->url]);
+        $view->registerMetaTag(['property' => 'og:image', 'content' => $this->image]);
+        $view->registerMetaTag(['property' => 'og:image:width', 'content' => 1024]);
+        $view->registerMetaTag(['property' => 'og:image:height', 'content' => 512]);
 
         //Twitter
-        $this->getView()->registerMetaTag(['name' => 'twitter:card', 'content' => 'summary_large_image']);
-        $this->getView()->registerMetaTag(['name' => 'twitter:title', 'content' => $this->title]);
-        $this->getView()->registerMetaTag(['name' => 'twitter:description', 'content' => $this->description]);
-        $this->getView()->registerMetaTag(['name' => 'twitter:image', 'content' => $this->image]);
+        $view->registerMetaTag(['name' => 'twitter:card', 'content' => 'summary_large_image']);
+        $view->registerMetaTag(['name' => 'twitter:title', 'content' => $this->title]);
+        $view->registerMetaTag(['name' => 'twitter:description', 'content' => $this->description]);
+        $view->registerMetaTag(['name' => 'twitter:image', 'content' => $this->image]);
     }
 }
