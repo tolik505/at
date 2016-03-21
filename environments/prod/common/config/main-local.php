619190d108b707df2abe3cd5d330b14b933a371e
pavelnaumenko <pavel@vintage.com.ua>
2016-03-21 12:38:07 +0200

    add custom Schema for 'comment' method support inside migrations

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,10 +3,22 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=melon',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
+            'schemaMap' => [
+                'pgsql' => 'yii\db\pgsql\Schema', // PostgreSQL
+                'mysqli' => '\common\components\Schema', // MySQL
+                'mysql' => '\common\components\Schema', // MySQL
+                'sqlite' => 'yii\db\sqlite\Schema', // sqlite 3
+                'sqlite2' => 'yii\db\sqlite\Schema', // sqlite 2
+                'sqlsrv' => 'yii\db\mssql\Schema', // newer MSSQL driver on MS Windows hosts
+                'oci' => 'yii\db\oci\Schema', // Oracle driver
+                'mssql' => 'yii\db\mssql\Schema', // older MSSQL driver on MS Windows hosts
+                'dblib' => 'yii\db\mssql\Schema', // dblib drivers on GNU/Linux (and maybe other OSes) hosts
+                'cubrid' => 'yii\db\cubrid\Schema', // CUBRID
+            ]
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
