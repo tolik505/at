2b0ce13f01dd44b27bc4128a55e58c1f1498905e
hanterrian <hanterrian@gmail.com>
2016-03-23 11:44:24 +0200

    Add www rewrite

--- frontend/components/FrontendController.php
+++ frontend/components/FrontendController.php
@@ -6,10 +6,12 @@
 namespace frontend\components;
 
 
+use yii\helpers\ArrayHelper;
 use yii\helpers\Url;
 use yii\web\Controller;
 
-class FrontendController extends Controller {
+class FrontendController extends Controller
+{
 
     /**
      * @inheritdoc
@@ -17,6 +19,18 @@ class FrontendController extends Controller {
     public function beforeAction($action)
     {
         if (parent::beforeAction($action)) {
+            $url = \Yii::$app->request->getAbsoluteUrl();
+
+            preg_match("/(http|https):\/\/(www.)*/", $url, $match);
+
+            if (count($match)) {
+                if (ArrayHelper::getValue($match, 2) == 'www.') {
+                    $url = str_replace('www.', '', $url);
+
+                    return $this->redirect($url, 301);
+                }
+            }
+
             $this->view->registerLinkTag(['rel' => 'canonical', 'href' => Url::canonical()]);
             return true;
         } else {
